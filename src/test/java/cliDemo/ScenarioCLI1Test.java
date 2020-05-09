2a1e634e0cab400b3a76673782cc037fd4cbcdd2
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-05-09 05:35:26 +0200

    updating dd_system port

--- src/test/java/cliDemo/ScenarioCLI1Test.java
+++ src/test/java/cliDemo/ScenarioCLI1Test.java
@@ -225,7 +225,7 @@ public class ScenarioCLI1Test {
     private static void initialize() {
 //        t
         String host = "localhost";
-        String port = "8000";
+        String port = "8080";
         initCWS(host, port);
         initPWS(host, port);
         initPackWS(host, port);
